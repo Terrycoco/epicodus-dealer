2c9a2640d9b507db2872d2d998165577fabb5046
Evan Clough & Terry Marr <student@epicodus.com>
2015-08-12 10:32:18 -0700

    Write second spec. It will test that unique ID is created and iterated for each initilization of new Car object.

--- spec/dealer_spec.rb
+++ spec/dealer_spec.rb
@@ -3,10 +3,20 @@ require('car')
 require('pry')
 
 describe('Car') do
+
   describe('#initialize') do
     it('create a new car with properties make, model, year, id') do
       test_car = Car.new('Toyota', 'Celica', 2000)
       expect(test_car.model()).to(eq('Celica'))
     end
   end
+
+  describe(:create_id) do
+    it('generates a unique number based on the number of cars already initialized') do
+      Car.new('Ferrari', 'LaFerrari', 2014)
+      test_car = Car.new('Mercedes', 'S55 AMG', 2016)
+      expect(test_car.id()).to(eq(2))
+    end
+  end
+
 end
