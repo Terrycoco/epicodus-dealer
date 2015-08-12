4a7d0fa63415f45880bc69657218294da7a821cf
Evan Clough & Terry Marr <student@epicodus.com>
2015-08-12 15:15:41 -0700

    integration spec to click on dealership link and open dealership detail page

--- spec/dealer_integration_spec.rb
+++ spec/dealer_integration_spec.rb
@@ -1,8 +1,13 @@
 require('capybara/rspec')
 require('./app.rb')
+require('launchy')
 
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+before () do
+  Dealership.clear
+end
 
 describe('dealer test path', {:type => :feature}) do
  it('serves the index page which will ask the user to input a dealership') do
@@ -13,4 +18,19 @@ describe('dealer test path', {:type => :feature}) do
    click_button('Add')
    expect(page).to have_content('Bob Cars')
  end
+
+ it('Creates two dealerships and clicks on one to display a detail page') do
+   visit('/')
+   fill_in('name', :with => 'Bob Cars')
+   fill_in('city', :with => 'New York')
+   fill_in('state', :with => 'NY')
+   click_button('Add')
+   fill_in('name', :with => 'Robert Cars')
+   fill_in('city', :with => 'Newark')
+   fill_in('state', :with => 'NJ')
+   click_button('Add')
+   click_link('Robert Cars')
+   expect(page).to have_content('Newark')
+ end
+
 end
